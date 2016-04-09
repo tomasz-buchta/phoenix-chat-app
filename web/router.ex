62889fa76348bff5c424139f64554bc9f8537b4e
Tomasz Buchta <tomasz.buchta@hotmail.com>
2016-04-09 19:52:54 +0200

    remove needless pages

--- web/router.ex
+++ web/router.ex
@@ -17,7 +17,7 @@ defmodule ChatApp.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    resources "/messages", MessageController
+    get "/messages", MessageController, :index
   end
 
   # Other scopes may use custom stacks.
