09f9ead09137cc084b20c6cfb61fdff6d8c5c647
Tomasz Buchta <tomasz.buchta@hotmail.com>
2016-04-09 19:34:37 +0200

    basic chat functionality

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -1,23 +1,49 @@
-// NOTE: The contents of this file will only be executed if
-// you uncomment its entry in "web/static/js/app.js".
-
-// To use Phoenix channels, the first step is to import Socket
-// and connect at the socket path in "lib/my_app/endpoint.ex":
-import {Socket} from "phoenix"
-import $ from 'jquery'
+import {Socket} from "phoenix";
+import $ from "jquery";
 
 class SocketHandler {
-  static init(){
-    let socket = new Socket("/socket", {});
-    socket.connect();
+    static init() {
+        let socket = new Socket("/socket", {});
+        socket.connect();
 
-    // Now that you are connected, you can join channels with a topic:
-    let channel = socket.channel("rooms:lobby", {});
-    channel.join()
-        .receive("ok", resp => { console.log("Joined successfully", resp) })
-        .receive("error", resp => { console.log("Unable to join", resp) })
-  }
+        var $status = $("#status");
+        var $messages = $("#messages");
+        var $input = $("#message-input");
+        // Now that you are connected, you can join channels with a topic:
+        let channel = socket.channel("rooms:lobby", {});
+        channel.join()
+            .receive("ok", resp => {
+                console.log("Joined successfully", resp)
+            })
+            .receive("error", resp => {
+                console.log("Unable to join", resp)
+            });
+        channel.onError(e => console.log('something went wrong', e));
+        channel.onClose(e => console.log('Channel closed', e));
+
+        //send new message if user presses enter
+        $input.off('keypress').on('keypress', e => {
+            if (e.keyCode == 13) {
+                channel.push('shout', {content: $input.val()});
+                $input.val('');
+            }
+        });
+
+        //render new messages on clients
+        channel.on('shout', message => {
+            $messages.append(this.messageTemplate(message))
+        })
+    }
+
+    static sanitize(html) {
+        return $("<div/>").text(html).html()
+    }
+
+    static messageTemplate(msg) {
+        let content = this.sanitize(msg.content);
+        return (`<p>${content}</p>`)
+    }
 }
 
-$( () => SocketHandler.init() );
+$(() => SocketHandler.init());
 export default SocketHandler
