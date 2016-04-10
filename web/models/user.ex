6940adcf6ed91635f2935f4605d70ab436345aac
Tomasz Buchta <tomasz.buchta@hotmail.com>
2016-04-10 23:34:36 +0200

    User model scaffold

---
+++ web/models/user.ex
@@ -0,0 +1,26 @@
+defmodule ChatApp.User do
+  use ChatApp.Web, :model
+
+  schema "users" do
+    field :first_name, :string
+    field :last_name, :string
+    field :email, :string
+    field :encrypted_password, :string
+
+    timestamps
+  end
+
+  @required_fields ~w(first_name last_name email encrypted_password)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+end
